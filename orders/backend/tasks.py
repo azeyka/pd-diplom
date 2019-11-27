eb7c1641e87f5c1801cd59c5f5dec150268f297a
Elena <www.azeyka@mail.ru>
2019-11-27 15:26:46 +0300

    Create tasks.py

---
+++ orders/backend/tasks.py
@@ -0,0 +1,22 @@
+from celery import shared_task
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+
+@shared_task
+def send_email(title, body, to, files_path=None):
+    email = EmailMessage(
+        title,
+        body,
+        f'Магазин <{settings.EMAIL_HOST_USER}>',
+        to
+    )
+
+    if files_path:
+        for file_path in files_path:
+            email.attach_file(file_path)
+
+    email.send()
