81ecee03ce3499d5102cedfeda83bb64e4cb0b59
Elena <www.azeyka@mail.ru>
2019-12-09 13:14:04 +0300

    Update celery.py

--- orders/orders/celery.py
+++ orders/orders/celery.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orders.settings')
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 
 app = Celery('orders')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
