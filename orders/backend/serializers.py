872a525d60416d7440d1e5194d432e9ceb49dc6c
Elena <www.azeyka@mail.ru>
2019-12-19 11:47:54 +0300

    Update serializers.py

--- orders/backend/serializers.py
+++ orders/backend/serializers.py
@@ -16,20 +16,25 @@ class ShopSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     shop = ShopSerializer(read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'company', 'position', 'type', 'shop')
+        fields = ('id', 'username', 'email', 'first_name',
+                  'last_name', 'company', 'position', 'type', 'shop')
 
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ('id', 'user', 'city', 'street', 'house', 'structure', 'building', 'apartment', 'phone')
+        fields = ('id', 'user', 'city', 'street', 'house',
+                  'structure', 'building', 'apartment', 'phone')
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
+
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
@@ -55,30 +60,37 @@ class ProductInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductInfo
-        fields = ('id', 'external_id', 'model', 'product', 'shop', 'quantity', 'price', 'price_rrc', 'product_parameters',)
+        fields = ('id', 'external_id', 'model', 'product', 'shop',
+                  'quantity', 'price', 'price_rrc', 'image', 'product_parameters',)
         read_only_fields = ('id',)
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     product_info = ProductInfoSerializer(read_only=True)
+
     class Meta:
         model = CartItem
         fields = ('id', 'product_info', 'quantity',)
         read_only_fields = ('id',)
 
+
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(read_only=True, many=True)
+
     class Meta:
         model = Cart
         fields = ('id', 'items', )
         read_only_fields = ('id',)
 
+
 class OrderItemSerializer(serializers.ModelSerializer):
     product_info = ProductInfoSerializer(read_only=True)
+
     class Meta:
         model = OrderItem
         fields = ('id', 'product_info', 'quantity')
         read_only_fields = ('id',)
+
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
@@ -86,9 +98,11 @@ class OrderSerializer(serializers.ModelSerializer):
     shop = ShopSerializer(read_only=True)
     state = serializers.CharField(source='get_state_display')
     user = UserSerializer(read_only=True)
+
     class Meta:
         model = Order
-        fields = ('id', 'user', 'dt', 'state', 'contact', 'shop', 'items', 'total_summ')
+        fields = ('id', 'user', 'dt', 'state', 'contact',
+                  'shop', 'items', 'total_summ')
         read_only_fields = ('id',)
 
     def get_state(self, obj):
