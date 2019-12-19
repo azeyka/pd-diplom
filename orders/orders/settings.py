f9fd12ab04cda77646b17d01993df8411ae48036
Elena <www.azeyka@mail.ru>
2019-12-19 11:48:05 +0300

    Update settings.py

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -166,6 +166,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Celery
 
